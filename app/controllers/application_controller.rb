4e1caa469e1de4a9416c9aa2983cb220f1dfa50c
John Chen <chenjiapeng12345@gmail.com>
2018-08-17 10:55:24 -0400

    add user authentication for some sections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
   def logged_in?
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   helper_method :logged_in?
+
+  private
+  def require_login
+    unless logged_in?
+      redirect_to login_url, alert: 'You must be logged in to access this section'
+    end
+  end
 end
