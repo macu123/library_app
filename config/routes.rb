50b3bbc3092531ff5b2d702a55cb14a09d5f6ff0
John Chen <chenjiapeng12345@gmail.com>
2018-08-17 01:01:39 -0400

    add user login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  
   root to: 'books#index'
   get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
