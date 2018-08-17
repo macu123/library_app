13d98dac4d0a5de3c1534d228c382a68da0c8d58
John Chen <chenjiapeng12345@gmail.com>
2018-08-17 10:31:55 -0400

    add add_copies method to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,10 @@ class Book < ApplicationRecord
   def available?
     copies.where(available: true).count > 0
   end
+
+  def add_copies(num_of_copies)
+    num_of_copies.times do 
+      copies.create
+    end
+  end
 end
