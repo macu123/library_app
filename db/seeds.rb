cb529054b6631ac0e24302201f8cbf122eb14df1
John Chen <chenjiapeng12345@gmail.com>
2018-08-17 17:57:48 -0400

    seed all test books

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+Loan.destroy_all
+Copy.destroy_all
+Book.destroy_all
+User.destroy_all
+
+User.create(
+    email: 'admin@gmail.com', password: '12345',
+    password_confirmation: '12345', admin: true
+  )
+
+CSV.foreach(File.join(Rails.root, 'db', 'import', 'test_books.csv'), headers: true) do |row|
+  book = Book.create(
+      title: row['Title'], author: row['Author'], genre: row['Genre'],
+      sub_genre: row['SubGenre'], num_of_pages: row['Pages'].to_i,
+      publisher: row['Publisher']
+    )
+
+  row['Copies'].to_i.times { book.copies.create }
+end
