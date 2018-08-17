6692d7466be2d5175c259bb50e8c7e19139e80c2
John Chen <chenjiapeng12345@gmail.com>
2018-08-17 00:18:20 -0400

    add methods to loan

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,4 +1,28 @@
 class Loan < ApplicationRecord
   belongs_to :user
   belongs_to :copy
+
+  def book_title
+    copy.book.title
+  end
+
+  def book_author
+    copy.book.author
+  end
+
+  def book_genre
+    copy.book.genre
+  end
+
+  def book_sub_genre
+    copy.book.sub_genre
+  end
+
+  def book_num_of_pages
+    copy.book.num_of_pages
+  end
+
+  def book_publisher
+    copy.book.publisher
+  end
 end
