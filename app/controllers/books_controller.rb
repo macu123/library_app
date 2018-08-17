e4ab6a89f38daffb3f27b761825afb3fdf959ba7
John Chen <chenjiapeng12345@gmail.com>
2018-08-17 02:45:42 -0400

    add books#index and show page

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,43 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'It has been created successfully!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update_attributes(book_params)
+      redirect_to @book, notice: 'It has been updated successfully!'
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(
+        :title, :author, :genre, :sub_genre,
+        :num_of_pages, :publisher
+      )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   resources :sessions
   resources :loans
   resources :copies
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
